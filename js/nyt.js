7c73baa4785a913cdc0530d5b5929834eb75bc2c
Chris McGuire <cdmcguire000@gmail.com>
2019-02-26 20:29:03 -0800

    app is working

---
+++ js/nyt.js
@@ -0,0 +1,41 @@
+// API Key: c64d0a52048b48ec8c6f20d3f4930160
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+// add the API key to the URL
+url += '?' + jQuery.param({
+  'api-key': "RLaf4nOMnhGskphnPKqEQoYGLsMuO4Cg"
+});
+jQuery(document).ready(function(){
+    // GET data from the API
+    jQuery.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(data) {
+        console.log(data);
+        // select the div where the infomrtion will be displayed
+        var artSection = jQuery('.nyt-top-stories-container');
+        /*
+            Function takes the nyt data and builds a string to display the infromation
+        */
+        function displayNYTData(i, data) {
+            var displayString = "<article class='nytArticle'>";
+            displayString += "<h3 class='nytMainHeader'><a href="+ data.results[i].url +">"+data.results[i].title +" </a></h3>";
+            displayString += "<p class='abstract'>"+ data.results[i].abstract +"</p>";
+            displayString += "<h4 class='byLine'>"+ data.results[i].byline +"</h4>";
+            // Check for an image
+            //if(typeof data.results[i].multimedia[0] !== 'undefined') {
+            if(data.results[i].multimedia.length > 0) {
+                displayString += "<img class='nytImg' src="+ data.results[i].multimedia[3].url +" />";
+            }else{
+                displayString += "<p class='noImageFound'>Not Available</p>";
+            }
+            displayString += "<div class='clearDiv'></div>";
+            displayString += "</article>";
+            artSection.append(displayString);
+        }
+        for (var i=0 ; i<10 ; i++) {
+            displayNYTData(i, data);
+        }
+    }).fail(function(err) {
+        throw err;
+    });
+}); // end document ready
---
+++ nyt-top-stories.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Plugin Name: New York Times Top Stories
+Plugin URI: https://github.com/c-mcguire/NYT-Top-Stories
+Description: Widget that shows the current top stories from the New York Times
+Author: chris@new-volume.com
+Version: 1.0
+*/
+// Block direct requests
+// This prevents somebody from opening the URL directly to the widget itself
+if ( !defined('ABSPATH') )
+	die('-1');
+// Register the widget
+// See Codex https://codex.wordpress.org/Function_Reference/register_widget
+// Takes a class as a parameter
+add_action( 'widgets_init', function(){
+     register_widget( 'NYT_Top_Stories' );
+});
+function nyt_app() {
+    // Css for the plugins
+    wp_enqueue_style('nyt style', plugin_dir_url( __FILE__ ) .'/css/nyt.css');
+    // Javascript to populate the element with information
+    wp_enqueue_script( 'nyt_api_call', plugin_dir_url( __FILE__ ) . '/js/nyt.js', true );
+    $appDiv = "<div id='nyt-app-container' class='nyt-top-stories-container'></div>";
+    return $appDiv;
+}
+add_shortcode('nyt_top_stories', 'nyt_app');
+/**
+ * Adds My_Widget widget.
+ */
+// Codex: https://developer.wordpress.org/reference/classes/wp_widget/
+class NYT_Top_Stories extends WP_Widget {
+	/**
+	 * Register widget with WordPress.
+	 */
+	// Class constructor
+	function __construct() {
+		parent::__construct(
+			'NYT_Top_Stories', // Base ID
+			__('NYT Top Stories', 'text_domain'), // Name
+			array( 'description' => __( 'Widget that shows the current top stories from the New York Times', 'text_domain' ), ) // Args
+		);
+	}
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		// Codex: https://codex.wordpress.org/Widgets_API#Example
+     	echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+
+	}
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		// Check to see if any of the form controls are set already.
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php
+	}
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : 'off';
+		return $instance;
+	}
+} // class My_Widget
